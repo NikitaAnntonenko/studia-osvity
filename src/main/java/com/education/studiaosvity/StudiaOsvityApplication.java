91693897d6c097bf2d8928ccc113375d6005d411
nikitaantonenko <nikita.anntonenko@gmail.com>
2025-08-01 22:47:47 +0200

    Init project

---
+++ src/main/java/com/education/studiaosvity/StudiaOsvityApplication.java
@@ -0,0 +1,13 @@
+package com.education.studiaosvity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiaOsvityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudiaOsvityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/education/studiaosvity/StudiaOsvityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.studiaosvity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiaOsvityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
